e82c36fcab2660ff22da73f65397689dd3efbbba
Oleksandr Fedotov <fedotov4815162342@gmail.com>
2017-01-21 00:20:38 +0100

    cleaning serverside code

--- ftpserverside.py
+++ ftpserverside.py
@@ -1,6 +1,6 @@
 import socket, sys, os, threading, time
 
-listenAddr = None
+listenAddr = '10.1.0.3'
 listenPort = 80
 
 def log(message, clientAddr = None):
@@ -44,7 +44,7 @@ class FTPServer(threading.Thread):
         self.clientAddr = clientAddr
         self.dataListenSock = None
         self.dataSock = None
-        self.dataAddr = '127.0.0.1'
+        self.dataAddr = '10.1.0.3'
         self.dataPort = None
         self.username = ''
         self.authenticated = False
@@ -59,40 +59,19 @@ class FTPServer(threading.Thread):
                 self.controlSock.close()
                 log('Client disconnected.', self.clientAddr)
                 break
-            log('[' + (self.username if self.authenticated else '') + '] ' + cmd.strip(), self.clientAddr)
+
             cmdHead = cmd.split()[0].upper()
             if cmdHead == 'QUIT': # QUIT
                 self.controlSock.send(b'221 Service closing control connection. Logged out if appropriate.\r\b')
                 self.controlSock.close()
                 log('Client disconnected.', self.clientAddr)
                 break
-            elif cmdHead == 'HELP': # HELP
-                self.controlSock.send(b'214 QUIT HELP USER PASS PWD CWD TYPE PASV NLST RETR STOR\r\n')
-            elif cmdHead == 'USER': # USER
-                if len(cmd.split()) < 2:
-                    self.controlSock.send(b'501 Syntax error in parameters or arguments.\r\n')
-                else:
-                    self.username = cmd.split()[1]
-                    self.controlSock.send(b'331 User name okay, need password.\r\n')
-                    self.authenticated = False
-            elif cmdHead == 'PASS': # PASS
-                if self.username == '':
-                    self.controlSock.send(b'503 Bad sequence of commands.\r\n')
-                else:
-                    if len(cmd.split()) < 2:
-                        self.controlSock.send(b'501 Syntax error in parameters or arguments.\r\n')
-                    else:
-                        self.controlSock.send(b'230 User logged in, proceed.\r\n')
-                        self.authenticated = True
+
             elif cmdHead == 'PWD': # PWD
-                if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
-                else:
-                    self.controlSock.send(('257 "%s" is the current directory.\r\n' % self.cwd).encode('ascii'))
+                self.controlSock.send(('257 "%s" is the current directory.\r\n' % self.cwd).encode('ascii'))
+
             elif cmdHead == 'CWD': # CWD
-                if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
-                elif len(cmd.split()) < 2:
+                if len(cmd.split()) < 2:
                     self.controlSock.send(('250 "%s" is the current directory.\r\n' % self.cwd).encode('ascii'))
                 else:
                     programDir = os.getcwd()
@@ -106,34 +85,30 @@ class FTPServer(threading.Thread):
                         self.cwd = os.getcwd()
                         self.controlSock.send(('250 "%s" is the current directory.\r\n' % self.cwd).encode('ascii'))
                     os.chdir(programDir)
+
             elif cmdHead == 'TYPE': # TYPE, currently only I is supported
-                if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
-                elif len(cmd.split()) < 2:
+                if len(cmd.split()) < 2:
                     self.controlSock.send(b'501 Syntax error in parameters or arguments.\r\n')
                 elif cmd.split()[1] == 'I':
                     self.typeMode = 'Binary'
                     self.controlSock.send(b'200 Type set to: Binary.\r\n')
                 else:
                     self.controlSock.send(b'504 Command not implemented for that parameter.\r\n')
+
             elif cmdHead == 'PASV': # PASV, currently only support PASV
-                if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
-                else:
-                    if self.dataListenSock != None: # Close existing data connection listening socket
-                        self.dataListenSock.close()
-                    self.dataListenSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-                    self.dataListenSock.bind((self.dataAddr, 0))
-                    self.dataPort = self.dataListenSock.getsockname()[1]
-                    self.dataListenSock.listen(5)
-                    self.dataMode = 'PASV'
-                    DataSockListener(self).start()
-                    time.sleep(0.5) # Wait for connection to set up
-                    self.controlSock.send(('227 Entering passive mode (%s,%s,%s,%s,%d,%d)\r\n' % (self.dataAddr.split('.')[0], self.dataAddr.split('.')[1], self.dataAddr.split('.')[2], self.dataAddr.split('.')[3], int(self.dataPort / 256), self.dataPort % 256)).encode('ascii'))
+                if self.dataListenSock != None: # Close existing data connection listening socket
+                    self.dataListenSock.close()
+                self.dataListenSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+                self.dataListenSock.bind((self.dataAddr, 0))
+                self.dataPort = self.dataListenSock.getsockname()[1]
+                self.dataListenSock.listen(5)
+                self.dataMode = 'PASV'
+                DataSockListener(self).start()
+                time.sleep(0.5) # Wait for connection to set up
+                self.controlSock.send(('227 Entering passive mode (%s,%s,%s,%s,%d,%d)\r\n' % (self.dataAddr.split('.')[0], self.dataAddr.split('.')[1], self.dataAddr.split('.')[2], self.dataAddr.split('.')[3], int(self.dataPort / 256), self.dataPort % 256)).encode('ascii'))
+
             elif cmdHead == 'NLST': # NLST
-                if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
-                elif self.dataMode == 'PASV' and self.dataSock != None: # Only PASV implemented
+                if self.dataMode == 'PASV' and self.dataSock != None: # Only PASV implemented
                     self.controlSock.send(b'125 Data connection already open. Transfer starting.\r\n')
                     directory = '\r\n'.join(os.listdir(self.cwd)) + '\r\n'
                     self.dataSock.send(directory.encode('ascii'))
@@ -142,10 +117,9 @@ class FTPServer(threading.Thread):
                     self.controlSock.send(b'225 Closing data connection. Requested file action successful (for example, file transfer or file abort).\r\n')
                 else:
                     self.controlSock.send(b"425 Can't open data connection.\r\n")
+
             elif cmdHead == 'RETR':
-                if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
-                elif len(cmd.split()) < 2:
+                if len(cmd.split()) < 2:
                     self.controlSock.send(b'501 Syntax error in parameters or arguments.\r\n')
                 elif self.dataMode == 'PASV' and self.dataSock != None: # Only PASV implemented
                     programDir = os.getcwd()
@@ -162,10 +136,9 @@ class FTPServer(threading.Thread):
                     os.chdir(programDir)
                 else:
                     self.controlSock.send(b"425 Can't open data connection.\r\n")
+
             elif cmdHead == 'STOR':
-                if not self.authenticated:
-                    self.controlSock.send(b'530 Not logged in.\r\n')
-                elif len(cmd.split()) < 2:
+                if len(cmd.split()) < 2:
                     self.controlSock.send(b'501 Syntax error in parameters or arguments.\r\n')
                 elif self.dataMode == 'PASV' and self.dataSock != None: # Only PASV implemented
                     programDir = os.getcwd()
